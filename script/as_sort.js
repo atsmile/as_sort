964e27c3305a3e68653198af8d998de4ad1f9d31
atsmile <atsmile8723@gmail.com>
2020-11-08 07:42:24 +0900

    ファンクション化とコード整形

--- script/as_sort.js
+++ script/as_sort.js
@@ -5,62 +5,58 @@
 ・ボタンの設定 (クラスがあっていればタグは問わない)
 <li class="sort_btn" data-type="index" data-order="asc">元に戻す</li>
 data-type : ソートしたい項目 => index
-data-order : ソートの順序 => asc (昇順) / desc (降順) | 未設定:昇順
+data-order : ソートの順序 => asc (昇順) / desc (降順) | 未設定:昇順 | 以外の文字列:ソートしない
 data-target : 同一ページ内に複数設置したい時、ソートしたいリスト(ul)のクラス | 未設定:"js-sort-main"
 
 ・リスト (ul li) MEMO 後々タグ自由化予定
 data-targetで設定したクラスか、sort_mainをulに設定したリスト
 data-typeで設定したデータ要素("index"の場合data-index)の値でソートする
 */
-document.addEventListener(
-  'DOMContentLoaded',
-  function() {
-    const node_sort_btn =document.querySelectorAll('.js-sort-btn')
-    const sort_btn = Array.prototype.slice.call(node_sort_btn, 0); 
-    sort_btn.forEach(function(e) {
-      e.addEventListener('click', function() {
-        const target = e.getAttribute('data-target') || 'js-sort-main'
-        const tmp_parent = document.getElementsByClassName(target)
-        const dom_parent = tmp_parent[0]
-        const sort_type = 'data-' + e.getAttribute('data-type')
-        const children = dom_parent.children
+function as_sort() {
+  const node_sort_btn = document.querySelectorAll(".js-sort-btn");
+  const sort_btn = Array.prototype.slice.call(node_sort_btn, 0);
+  sort_btn.forEach(function (e) {
+    e.addEventListener("click", function () {
+      const target = e.getAttribute("data-target") || "js-sort-main";
+      const tmp_parent = document.getElementsByClassName(target);
+      const dom_parent = tmp_parent[0];
+      const sort_type = "data-" + e.getAttribute("data-type");
+      const children = dom_parent.children;
 
-        // 該当する data の番号をキーに連想配列にしてから配列に格納
-        var arr_sort_item = new Array()
-        const arr_children = Array.prototype.slice.call(children,0); 
-        arr_children.forEach(function(li_item) {
-          dom_parent.removeChild(li_item) // DOMの削除
-          var dom_data = {
-            key: parseInt(li_item.getAttribute(sort_type), 10),
-            value: li_item,
-          }
-          var data = Array()
-          const li_attr = Array.prototype.slice.call(li_item.attributes,0); 
-          li_attr.forEach(function(data_name) {
-            data[data_name.name] = data_name.value
-          })
-          dom_data['data'] = data
-          arr_sort_item.push(dom_data)
-        })
-        // ソート
-        func_sort(arr_sort_item, e.dataset.order)   
+      // 該当する data の番号をキーに連想配列にしてから配列に格納
+      var arr_sort_item = new Array();
+      const arr_children = Array.prototype.slice.call(children, 0);
+      arr_children.forEach(function (li_item) {
+        dom_parent.removeChild(li_item); // DOMの削除
+        var dom_data = {
+          key: parseInt(li_item.getAttribute(sort_type), 10),
+          value: li_item,
+        };
+        var data = Array();
+        const li_attr = Array.prototype.slice.call(li_item.attributes, 0);
+        li_attr.forEach(function (data_name) {
+          data[data_name.name] = data_name.value;
+        });
+        dom_data["data"] = data;
+        arr_sort_item.push(dom_data);
+      });
+      // ソート
+      func_sort(arr_sort_item, e.dataset.order);
 
-        arr_sort_item.forEach(function(elem) {
-          var new_elem = document.createElement('li')
-          elem_data = elem.data // data 属性をすべて格納
-          Object.keys(elem_data).forEach(function (type) {
-            new_elem.setAttribute([type], elem_data[type])
-          })
-          new_elem.innerHTML = elem.value.innerHTML
-          dom_parent.appendChild(new_elem)
-        })
-      })
-    })
-  },
-  false
-)
+      arr_sort_item.forEach(function (elem) {
+        var new_elem = document.createElement("li");
+        elem_data = elem.data; // data 属性をすべて格納
+        Object.keys(elem_data).forEach(function (type) {
+          new_elem.setAttribute([type], elem_data[type]);
+        });
+        new_elem.innerHTML = elem.value.innerHTML;
+        dom_parent.appendChild(new_elem);
+      });
+    });
+  });
+}
 
-// // ソート デフォルトで昇順
+// ソート デフォルトで昇順
 function func_sort(arr, args_order) {
   const order = args_order || "asc";
   arr.sort(function (a, b) {
@@ -79,3 +75,5 @@ function func_sort(arr, args_order) {
     }
   });
 }
+
+document.addEventListener("DOMContentLoaded", as_sort(), false);
